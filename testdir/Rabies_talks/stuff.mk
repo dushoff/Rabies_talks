9ef0b9964bf44f9599b79b59a268d6dd11a092b5
Jonathan Dushoff <dushoff@mcmaster.ca>
2015-12-15 17:48:01 -0500

    Working from the repo __if you have access to the Dropbox folder__.

---
+++ testdir/Rabies_talks/stuff.mk
@@ -0,0 +1,18 @@
+%.makestuff:
+	-cd $(dir $(ms)) && mv -f $(notdir $(ms)) .$(notdir $(ms))
+	cd $(dir $(ms)) && git clone $(msrepo)/$(notdir $(ms)).git
+	-cd $(dir $(ms)) && rm -rf .$(notdir $(ms))
+	touch $@
+
+msrepo = https://github.com/dushoff
+gitroot = ./
+Drop = ~/Dropbox
+
+-include local.mk
+-include $(gitroot)/local.mk
+export ms = $(gitroot)/makestuff
+
+Makefile: $(ms)
+
+$(ms):
+	cd $(dir $(ms)) && git clone $(msrepo)/$(notdir $(ms)).git
